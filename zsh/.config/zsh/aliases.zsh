3751a7eafc0f14b661c8d67ed7672ef399054d0e
Dakota Knutson <dakota.knutson@evisort.com>
2024-02-02 17:14:05 -0800

    Add osx quick look preview alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias dc=docker-compose
 alias ll="ls -al"
 alias ls="ls --color=auto"
 alias python=python3
+alias ql="qlmanage -p" # Quick Look
 alias tf=terraform
 alias tfw="terraform workspace select"
 alias v=vim
