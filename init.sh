f09d6041cf5b2a55371d7a75710114a220d208f5
Dakota Knutson <dakota.knutson@evisort.com>
2024-02-08 15:46:18 -0800

    Improve init script. Add nvim configs for stow.

--- init.sh
+++ init.sh
@@ -11,27 +11,30 @@ cd "$(dirname "$0")"
 
 # Install OSX Command Line Tools
 if ! xcode-select -v > /dev/null 2>&1; then
-  echo "Installing xcode-select"
+  echo "Installing xcode-select..."
   xcode-select --install
 fi
 
 
 # Check to see if brew is installed, and install it if it isn't
 if ! [ -x "$(command -v brew)" ]; then
-  echo 'Installing Homebrew'
+  echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 
+echo "Installing brew packages..."
 # Install brew packages
 brew bundle
 
 
 STOW_PACKAGES=(
-  zsh
   hammerspoon
+  nvim
+  zsh
 )
 
+echo "Deploying config dotfiles..."
 # Use stow to create symlinks for config files
 stow ${STOW_PACKAGES[@]}
 
@@ -40,5 +43,6 @@ stow ${STOW_PACKAGES[@]}
 source ~/.config/zsh/.zshrc
 
 
+echo "Updating hammerspoon default config location..."
 # Update Hammerspoon default config location
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
