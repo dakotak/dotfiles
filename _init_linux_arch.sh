a05c1b3909e73292ca40c385a90a2b2062e76cd7
Dakota Knutson <dakota.knutson@gmail.com>
2024-02-23 10:13:44 -0800

    Add yay for arch

--- _init_linux_arch.sh
+++ _init_linux_arch.sh
@@ -5,3 +5,16 @@ echo "Installing packages via pacman..."
 # Remove comments and blank lines from Pacfile pass the resulting list of packages to pacman
 sed '/^[[:blank:]]*#/d;/^[[:blank:]]*$/d;s/[[:blank:]]*#.*//' Pacfile | sudo pacman -S --needed -
 
+
+function install_yay {
+  pushd "$(mktemp --directory)" &> /dev/null || return 1
+  git clone https://aur.archlinux.org/yay.git .
+  makepkg -si
+  popd &> /dev/null || return 1
+}
+
+# YAY: Install yay to manage packages from Arch User Repository (AUR)
+if ! type yay &> /dev/null; then
+  echo "Installing yay..."
+  install_yay || echo "Error installing yay!"
+fi
