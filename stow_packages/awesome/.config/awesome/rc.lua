769394987215e1ec19b8e2547409cee3cf617025
Dakota Knutson <dakota.knutson@evisort.com>
2024-02-28 13:01:24 -0800

    Use local theme file

--- stow_packages/awesome/.config/awesome/rc.lua
+++ stow_packages/awesome/.config/awesome/rc.lua
@@ -49,7 +49,15 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+if not beautiful.init("theme.lua") then
+    naughty.notify({
+        preset = naughty.config.presets.critical,
+        title = "Oops, there were errors during startup!",
+        text = "Failed to load theme.lua"
+    })
+    -- Load default theme
+    beautiful.init(gears.filesystem.get_themes_dir() .. "dtheme.lua")
+end
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
